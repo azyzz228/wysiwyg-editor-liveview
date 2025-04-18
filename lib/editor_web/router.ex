a6791eccc1882828b6697289cff8cf5085ea1dd1
aziz abd <aziz.abdullaev@proton.me>
2025-04-18 13:04:18 +0500

    add blogs/new + blogs/:id

--- lib/editor_web/router.ex
+++ lib/editor_web/router.ex
@@ -18,6 +18,8 @@ defmodule EditorWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :home
+    live "/blogs/new", BlogCreateLive
+    live "/blogs/:id", BlogShowLive
   end
 
   # Other scopes may use custom stacks.
