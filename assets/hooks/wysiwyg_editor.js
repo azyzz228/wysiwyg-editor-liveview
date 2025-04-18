a5c28530468806ff3c617b6d4b9a9986ed624035
aziz abd <aziz.abdullaev@proton.me>
2025-04-18 13:03:36 +0500

    feat: text change event emittance, handle form submission

--- assets/hooks/wysiwyg_editor.js
+++ assets/hooks/wysiwyg_editor.js
@@ -1,7 +1,39 @@
+import Quill from "quill";
+var TurndownService = require("turndown");
 
 const WYSIWYG_editor = {
     mounted() {
-        alert(12)
+        const view = this;
+        const toolbarOptions = [
+          ["bold", "italic", "underline", "strike"], // toggled buttons
+          ["blockquote", "image", "link"],
+          [{ list: "ordered" }, { list: "bullet" }, { list: "check" }],
+          [{ header: [1, 2, 3, 4, 5, 6, false] }],
+          ["clean"], // remove formatting button
+        ];
+        const editor = new Quill("#editor", {
+          theme: "snow",
+          modules: { toolbar: toolbarOptions },
+        });
+
+        editor.on("text-change", () => {
+          view.pushEvent("content-text-change", { content: editor.getText() });
+        });
+
+        view.handleEvent("form_submitted", () => {
+          const html = editor.container.firstChild.innerHTML;
+
+          if (editor.getText() == "") {
+            view.pushEvent("editor_content_markdown", { message: "" });
+          } else {
+            var turndownService = new TurndownService();
+            const markdown = turndownService.turndown(html);
+            view.pushEvent("editor_content_markdown", { message: markdown });
+          }
+
+      });
+
+
     }
 }
 
